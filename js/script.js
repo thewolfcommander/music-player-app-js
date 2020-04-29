c3d8c796d9ff1d1432b3a5e6351ef657882d0609
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-29 21:25:18 +0530

    Sound is working on click

--- js/script.js
+++ js/script.js
@@ -3,17 +3,10 @@ window.addEventListener('load', () => {
     const pads = document.querySelectorAll('.pads div');
     const visual = document.querySelector('.visual');
 
-    const colors = [
-        '#60d394',
-        '#d36060',
-        '#c060d3',
-        '#d3d160',
-        '#6860d3',
-        '#27af98'
-    ]
+    const colors = ['#60d394', '#d36060', '#c060d3', '#d3d160', '#6860d3', '#27af98']
 
     pads.forEach((pad, index) => {
-        pad.addEventListener('click', function() {
+        pad.addEventListener('click', function () {
             sounds[index].currentTime = 0;
             sounds[index].play();
             createBubbles(index);
